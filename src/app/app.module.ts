5c267a43f23514c07e1f926b9f959eef828de0a0
Leo Albert <leo.albert.96@web.de>
2023-10-06 13:16:20 +0200

    added about me section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { LandingpageComponent } from './landingpage/landingpage.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LandingpageComponent
+    LandingpageComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
