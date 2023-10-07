e306af1b74c904f9ba290a91a8ad6d8c2c14c59e
Leo Albert <leo.albert.96@web.de>
2023-10-07 13:45:30 +0200

    created project component to display projects for testing

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
   styleUrls: ['./project.component.scss']
 })
-export class ProjectComponent {
+export class ProjectComponent implements OnInit {
 
+  @Input() projectInfo: any;
+  tech = [];
+  techname = '';
+
+  constructor(){}
+
+  ngOnInit(): void {
+   this.tech = this.projectInfo['tech'];
+   console.log(this.tech);
+  }
 }
