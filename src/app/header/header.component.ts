a503cf992e54c28ee3230e2e06a4a62c4c5d5279
Leo Albert <leo.albert.96@web.de>
2023-11-06 17:10:59 +0100

    responsive

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+  slideMenuShowing = false;
+
+  constructor(){}
+
+  ngOnInit(): void {
+    
+  }
+
+  showSlideMenu(){
+    let menu = document.getElementById('slideMenu');
+    if (this.slideMenuShowing == false) {
+      menu?.classList.add('show-slide-menu');
+      this.slideMenuShowing = true;
+    } else {
+      menu?.classList.remove('show-slide-menu');
+      this.slideMenuShowing = false;
+    }
+  }
 }
